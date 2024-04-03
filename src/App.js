526bf37089863740c4c69ac34897075e9d1d508b
Steve Berczuk <steve@berczuk.com>
2024-04-03 10:49:13 -0400

    handle clicks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {line_data} from './mbta-data'
+import ReactDOM from "react-dom";
 import {useState, useEffect} from 'react';
 
 function MyButton({count, onClick}) {
@@ -21,22 +22,21 @@ function MyButton({count, onClick}) {
 // why can't I pass the object with attributes down?
 // change the onclick handler to display a new component with the list of stops
 // refactor the handler to a higher  scope
-function RouteLine({ rec}) {
-    function displayDetails () {
-        alert('stops for: '+ rec.name)
-        return <StopsTable>stops</StopsTable>
-    }
+function RouteLine({rec, onSelect}) {
+    // change this to follow the model here:  https://react.dev/reference/react-dom/createPortal
+    // set state to reflect which line to show details for
+    // display only when not empty.
 
-    console.log("Route Line")
+
     return (
-        <div className={'wrapper'}>
-            <div>{rec.id}</div>
+        <li id={rec.id} key={rec.key} className={'wrapper'}>
+            <div key={rec.key}>{rec.id}</div>
 
-            <button onClick={displayDetails}
-                 style={{color: "#" + rec.color}}>{rec.name}</button>
-        </div>
+            <button onClick={onSelect}
+                    style={{color: "#" + rec.color}}>{rec.name}</button>
 
 
+        </li>
     );
 }
 
@@ -54,18 +54,22 @@ function useTime() {
 
 export default function MyApp() {
     const [count, setCount] = useState(0);
-    const time = useTime();
+    // const time = useTime();
 
     function handleClick() {
         setCount(count + 1);
     }
 
+
     return (
+
         <div>
             <h1>Welcome to my app</h1>
             <MyButton count={count} onClick={handleClick}/>
             <MyButton count={count} onClick={handleClick}/>
-            <Clock color={"red"} time={time.toLocaleTimeString()}/>
+            {/*<Clock color={"red"} time={time.toLocaleTimeString()}/>*/}
+
+            <div id={"details-div"}>Details here</div>
             <LinesTable
                 data={line_data.data}/>
         </div>
@@ -80,25 +84,55 @@ function Clock({color, time}) {
     );
 }
 
-function StopsTable({line}){
-    return (<div> WHOOPS!</div>)
+// Another option: Juyst have this at the  details div, and vary what's there  based on the click
+function StopsTable({line, onDone}) {
+    return (
+        <div>
+            <p>whoopos! {line}</p>
+            <button onClick={onDone}>Close</button>
+        </div>
+    )
 }
 
 function LinesTable({data}) {
+    const [selectedLine, setSelectedLine] = useState('');
+
+
     console.log(data)
     const rows = [];
     data.forEach((line) => {
-            console.log(line)
-            rows.push(<RouteLine
-                rec={{id: line.id, name: line.attributes.long_name, color: line.attributes.color}}/>)
+        console.log(line)
+        function handleSelect(){
+            console.log('sel ' + line.id)
+            setSelectedLine(line.id)
         }
+
+        rows.push(<RouteLine
+            rec={{
+                key: line.id, id: line.id, name: line.attributes.long_name,
+                color: line.attributes.color
+            }}
+            onSelect={handleSelect}/>)
+
+    })
+
+    // TODO: Why isn't this re-rendering after a state change?
+    return (
+        <>
+            <ul className={'main-container'}>
+                {rows}
+            </ul>
+            <div>{'selected: '+ selectedLine}</div>
+            {
+                selectedLine == "line-Red" && ReactDOM.createPortal(
+                    <div>
+                        <StopsTable line={selectedLine} onDone={() => setSelectedLine("")}/>
+                    </div>
+                    , document.getElementById('details-div'))
+            }
+        </>
     )
 
-    return (
-        <div className={'main-container'}>
-            {rows}
-        </div>
-    )
 }
 
 
